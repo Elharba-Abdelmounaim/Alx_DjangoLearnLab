021546ac1cae89de13a96529f9db8a9bfb7ca01e
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-07 11:04:12 +0100

    Fix: Use template rendering in list_books view and add missing templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,12 +6,11 @@ from .models import Book, Library
 
 #  دالة عرض بسيطة تعيد الكتب كنص عادي
 def list_books(request):
-    books = Book.objects.all()
-    text_list = "\n".join(f"{book.title} by {book.author.name}" for book in books)
-    return HttpResponse(text_list, content_type="text/plain")
+     books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # عرض صفّي باستخدام DetailView لمكتبة معينة
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
+     model = Library
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
