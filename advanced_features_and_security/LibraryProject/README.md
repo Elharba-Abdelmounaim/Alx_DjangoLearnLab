039c459a374721882f15476ed9e7fe3e22f97559
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-12 18:51:18 +0100

    1. Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -18,6 +18,22 @@ Alx_DjangoLearnLab/
 ├── delete.md
 └── CRUD_operations.md
 
-
-
 ---
+
+# Managing Permissions and Groups in Django
+
+## Permissions Defined
+- can_view: Allow viewing books
+- can_create: Allow creating new books
+- can_edit: Allow editing books
+- can_delete: Allow deletion of books
+
+## Groups
+- Viewers: Have only can_view permission
+- Editors: Have can_edit and can_create permissions
+- Admins: All permissions
+
+## Usage in Views
+- The @permission_required decorator was used to protect views based on the permissions granted.
+
+Translated with DeepL.com (free version)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
-class Book (models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+
     class Meta:
-        permissions= [
-            ("can_view", "can view book"),
-            ("can_create", "can create book"),
-            ("can_edit", "can edit book"),
-            ("can_delete", "can delete book"),
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
 
     def __str__(self):
