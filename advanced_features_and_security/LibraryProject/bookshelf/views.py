4ec42e1e84642bd164c48410a0ac09b58559ab77
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-12 19:03:17 +0100

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,39 +1,15 @@
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Book
-from .forms import BookForm
+# bookshelf/views.py
 from django.shortcuts import render
+from .forms import ExampleForm
+from .models import Book
 from django.db.models import Q
-from .forms import SearchForm
-
-
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
-@permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
-    
-    pass
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    
-    pass
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-   
-    pass
 
 def search_books(request):
-    form = SearchForm(request.GET or None)
+    form = ExampleForm(request.GET or None)
     books = Book.objects.none()
 
     if form.is_valid():
         query = form.cleaned_data['query']
-       
         books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
 
-    return render(request, 'bookshelf/search_results.html', {'form': form, 'books': books})
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
