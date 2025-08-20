b6466dc60c87684f9978ca7b53893daa89f26f39
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-20 15:59:24 +0100

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,27 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from .models import Post
-from accounts.models import CustomUser
+from .models import Post, Like
 from .serializers import PostSerializer
+from notifications.views import create_notification
 
-class FeedView(generics.GenericAPIView):
+class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
-        following_users = request.user.following.all()
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if created:
+            # إنشاء Notification
+            create_notification(recipient=post.author, actor=request.user, verb='liked', target=post)
+            return Response({'status': 'post liked'}, status=status.HTTP_201_CREATED)
+        return Response({'status': 'already liked'}, status=status.HTTP_200_OK)
 
-        feed_posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-        serializer = PostSerializer(feed_posts, many=True)
-        return Response(serializer.data)
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        deleted, _ = Like.objects.filter(user=request.user, post=post).delete()
+        if deleted:
+            return Response({'status': 'post unliked'}, status=status.HTTP_200_OK)
+        return Response({'status': 'not liked yet'}, status=status.HTTP_400_BAD_REQUEST)
