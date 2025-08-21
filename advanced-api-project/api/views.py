96bd7043d74606be2021220dfa7dd1d7b1c61d88
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-21 17:51:28 +0100

    Implementing Filtering, Searching, and Ordering in Django REST Framework mandatory

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+# --- CRUD Views for Book ---
+
+# List all books / Create new book
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Permissions: list = anyone, create = only authenticated
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+
+# Retrieve / Update / Delete single book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Permissions: update/delete = only authenticated
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
